c87cf4440af7a7d0db4c84f2f2fba0ea626a67d8
VidadiMurad <vidad0murad@gmail.com>
2026-05-21 16:10:02 +0400

    Task 1 Finish

--- Orient.17.Tasks/Orient.17.Tasks/main.swift
+++ Orient.17.Tasks/Orient.17.Tasks/main.swift
@@ -3,9 +3,23 @@
 //  Orient.17.Tasks
 //
 //  Created by Vidadi Muradzada on 21.05.26.
-//
+//  Collection Types Projects
 
 import Foundation
 
-print("Hello, World!")
+
+//                                              Task 1
+/*
+func arraySum(arr: [Int]) -> Int {
+    var sumOfNums: Int = 0
+    for i in arr {
+        sumOfNums = sumOfNums + i
+    }
+    return sumOfNums
+}
+
+print(arraySum(arr: [1,2,3,4,5,6]))
+ */
+//----------------------------------------------------
+
 
