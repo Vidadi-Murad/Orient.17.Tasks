4274bb6df7f6b3293fbb879fb7d4ea3e15cf7ec9
VidadiMurad <vidad0murad@gmail.com>
2026-05-21 16:25:21 +0400

    Task 2 Finish

--- Orient.17.Tasks/Orient.17.Tasks/main.swift
+++ Orient.17.Tasks/Orient.17.Tasks/main.swift
@@ -23,3 +23,21 @@ print(arraySum(arr: [1,2,3,4,5,6]))
 //----------------------------------------------------
 
 
+//                                              Task 2
+/*
+func greatOne(arr: [Int]) -> Int?{
+    if arr.isEmpty { return nil }
+    
+    var maxOne: Int = arr[0]
+    
+    for num in arr[1..<arr.count] {
+        if maxOne < num {
+            maxOne = num
+        }
+    }
+    return maxOne
+}
+
+print(greatOne(arr: [10,90,67,882,992])!)
+*/
+//----------------------------------------------------
